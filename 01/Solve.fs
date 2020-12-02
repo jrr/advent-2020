f98f36a2766cd3d97057b299a1d29e4cca61a1c2
John Ruble <john.ruble@atomicobject.com>
2020-12-02 14:56:11 -0500

    solve 01a

--- 01/Solve.fs
+++ 01/Solve.fs
@@ -1,5 +1,27 @@
 module Solve
 open System
+open System.Collections.Generic
 
-let solve (input:string) =
-    input
+    
+let rec findPairIn (sum:int) (input: LinkedList<int>) =
+    let first = input.First.Value
+    let last = input.Last.Value
+    
+    if first+last= sum then
+        first,last
+    else
+        if first+last < sum then
+            input.RemoveFirst()
+            findPairIn sum input
+        else
+            input.RemoveLast()
+            findPairIn sum input
+    
+let findPair (sum: int) (input: int list) =
+    let sorted = input |>List.sort
+    let ll = new LinkedList<int>(sorted) // cheating
+    findPairIn sum ll
+    
+let solve (input:int list) =
+    let (a,b) = findPair 2020 input
+    a * b
