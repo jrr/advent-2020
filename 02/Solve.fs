b52632ae2c8dd762519db7a1f61cd28cd64afd08
John Ruble <john.ruble@atomicobject.com>
2020-12-03 00:39:36 -0500

    solve 2a

--- 02/Solve.fs
+++ 02/Solve.fs
@@ -1,31 +1,43 @@
 module Solve
+
 open System
 
-let solve (input:string) =
-    input
+type Parsed =
+    { range: int * int
+      char: char
+      string: string }
 
-type Parsed = {
-    range: int*int
-    char: char
-    string: string
-}
-let parse (input:string) =
+let parse (input: string) =
     let what = input.Split ' ' |> List.ofArray
-    let a::b::c::tail = what
-    if tail.Length > 0 then
-        failwith "parse error"
-    let [a1;a2] = a.Split '-' |> List.ofArray
-    
-    {range=(int a1,int a2);char=b.[0];string=c}
-    
-let inRange (range: int*int) (input:int) =
-    fst range <= input && input <= snd range
-    
-let validate (record:Parsed) =
+    let a :: b :: c :: tail = what
+    if tail.Length > 0 then failwith "parse error"
+    let a1 :: a2 :: tail = a.Split '-' |> List.ofArray
+    if tail.Length > 0 then failwith "parse error"
+
+    { range = (int a1, int a2)
+      char = b.[0]
+      string = c }
+
+let inRange (range: int * int) (input: int) = fst range <= input && input <= snd range
+
+let validate (record: Parsed) =
     let counts = record.string |> Seq.groupBy id
-    let howManyOfTheSpecifiedChar = counts |> Seq.tryFind (fun (x,y) -> x = record.char)
-                                    |> function
-                                    | None -> 0
-                                    | Some x -> x |> snd |> Seq.length
+
+    let howManyOfTheSpecifiedChar =
+        counts
+        |> Seq.tryFind (fun (x, y) -> x = record.char)
+        |> function
+        | None -> 0
+        | Some x -> x |> snd |> Seq.length
+
     inRange record.range howManyOfTheSpecifiedChar
-                                    
+
+
+
+let solve (input: string) =
+    input.Split '\n'
+    |> Seq.map (fun l -> l.Trim())
+    |> Seq.filter (fun l -> l.Length <> 0)
+    |> Seq.map parse
+    |> Seq.filter validate
+    |> Seq.length
