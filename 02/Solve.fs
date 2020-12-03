b3131938b8580a9592a37df933e7db136d6e3b2d
John Ruble <john.ruble@atomicobject.com>
2020-12-03 00:31:29 -0500

    progress

--- 02/Solve.fs
+++ 02/Solve.fs
@@ -3,3 +3,29 @@ open System
 
 let solve (input:string) =
     input
+
+type Parsed = {
+    range: int*int
+    char: char
+    string: string
+}
+let parse (input:string) =
+    let what = input.Split ' ' |> List.ofArray
+    let a::b::c::tail = what
+    if tail.Length > 0 then
+        failwith "parse error"
+    let [a1;a2] = a.Split '-' |> List.ofArray
+    
+    {range=(int a1,int a2);char=b.[0];string=c}
+    
+let inRange (range: int*int) (input:int) =
+    fst range <= input && input <= snd range
+    
+let validate (record:Parsed) =
+    let counts = record.string |> Seq.groupBy id
+    let howManyOfTheSpecifiedChar = counts |> Seq.tryFind (fun (x,y) -> x = record.char)
+                                    |> function
+                                    | None -> 0
+                                    | Some x -> x |> snd |> Seq.length
+    inRange record.range howManyOfTheSpecifiedChar
+                                    
--- 02/Tests.fs
+++ 02/Tests.fs
@@ -9,3 +9,21 @@ open Solve
 let ``solves problem`` () =
     solve "foo" |> should equal "foo"
     
+let input1 =
+    """
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+    """
+    
+[<Fact>]
+let ``parses line`` () =
+    parse "1-3 a: abcde" |> should equal {range=(1,3);char='a';string="abcde"}
+
+    
+[<Fact>]
+let ``validates good`` () =
+    validate {range=(1,3);char='a';string="abcde"} |> should equal true
+[<Fact>]
+let ``validates bad`` () =
+    validate {range=(1,3);char='z';string="abcde"} |> should equal false
