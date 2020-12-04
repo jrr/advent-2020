ed460ee5fc7b94f202ec4bdefc7acb2f15e9155f
John Ruble <john.ruble@atomicobject.com>
2020-12-04 00:35:51 -0500

    solve

--- 04/Solve.fs
+++ 04/Solve.fs
@@ -6,23 +6,23 @@ open System
 let parse (input: string) =
     input.Split("\n\n")
     |> Seq.map (fun s -> s.Trim())
+    |> Seq.filter (fun s -> s.Length > 0)
     |> Seq.map (fun s ->
         s.Split([| ' '; '\n' |])
         |> Seq.map (fun s2 -> s2.Split(':'))
         |> Seq.map (fun a -> (a.[0], a.[1]))
         |> Array.ofSeq)
 
-let solve (input: string) =
-    let parsed = parse input
-    parsed
-    |> Seq.map (fun r ->
-        let keys =
-            r
+let validateHasAllFields (input: (string*string) seq) =
+    input 
             |> Seq.map fst
             |> Seq.sort
             |> Seq.filter (fun s -> s <> "cid")
             |> Seq.reduce (sprintf "%s-%s")
-
-        String.Equals(keys, "byr-ecl-eyr-hcl-hgt-iyr-pid"))
-    |> Seq.filter id
+            |> (fun s -> String.Equals(s, "byr-ecl-eyr-hcl-hgt-iyr-pid"))
+    
+let solve (input: string) =
+    let parsed = parse input
+    parsed
+    |> Seq.filter (validateHasAllFields)
     |> Seq.length
