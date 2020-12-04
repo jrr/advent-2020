4e6d568509afd9fdcf80b8adf82fe08bb1aba048
John Ruble <john.ruble@atomicobject.com>
2020-12-04 01:17:54 -0500

    4b

--- 04/Solve.fs
+++ 04/Solve.fs
@@ -13,16 +13,67 @@ let parse (input: string) =
         |> Seq.map (fun a -> (a.[0], a.[1]))
         |> Array.ofSeq)
 
-let validateHasAllFields (input: (string*string) seq) =
-    input 
-            |> Seq.map fst
-            |> Seq.sort
-            |> Seq.filter (fun s -> s <> "cid")
-            |> Seq.reduce (sprintf "%s-%s")
-            |> (fun s -> String.Equals(s, "byr-ecl-eyr-hcl-hgt-iyr-pid"))
-    
+let validateHasAllFields (input: (string * string) seq) =
+    input
+    |> Seq.map fst
+    |> Seq.sort
+    |> Seq.filter (fun s -> s <> "cid")
+    |> Seq.reduce (sprintf "%s-%s")
+    |> (fun s -> String.Equals(s, "byr-ecl-eyr-hcl-hgt-iyr-pid"))
+
+let yearBetween (s: string) (a: int) (b: int) =
+    s.Length = 4 && (int s) >= a && (int s) <= b
+
+
+open System.Text.RegularExpressions
+
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success
+    then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
+
+//    //Example:
+//    let phone = "(555) 555-5555"
+//    match phone with
+//    | Regex @"\(([0-9]{3})\)[-. ]?([0-9]{3})[-. ]?([0-9]{4})" [ area; prefix; suffix ] ->
+//        printfn "Area: %s, Prefix: %s, Suffix: %s" area prefix suffix
+//    | _ -> printfn "Not a phone number"
+
+let validateHeight (s: string) =
+    match s with
+    | Regex @"(\d+)cm" [ num ] -> (int num >= 150) && (int num <= 193)
+    | Regex @"(\d+)in" [ num ] -> (int num >= 59) && (int num <= 76)
+    | _ -> false
+
+let validateFieldValue (input: (string * string)): bool =
+    match input with
+    | "byr", v -> yearBetween v 1920 2002
+    | "iyr", v -> yearBetween v 2010 2020
+    | "eyr", v -> yearBetween v 2020 2030
+    | "hgt", v -> validateHeight v
+    | "hcl", Regex @"#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]" [] -> true
+    | "hcl", _ -> false
+    | "ecl", v -> ["amb";"blu";"brn";"gry";"grn";"hzl";"oth"] |> Seq.contains v
+    | "pid", Regex @"^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$" [] -> true
+    | "cid",_ -> true
+    | _ -> false
+
+let validateFieldValues (input: (string * string) seq): bool =
+    input
+    |> Seq.map validateFieldValue
+    |> Seq.contains false
+    |> not
+
 let solve (input: string) =
     let parsed = parse input
     parsed
     |> Seq.filter (validateHasAllFields)
+    |> Seq.length
+
+let solve2 (input: string) =
+    let parsed = parse input
+    parsed
+    |> Seq.filter (validateHasAllFields)
+    |> Seq.filter (validateFieldValues)
     |> Seq.length
--- 04/Tests.fs
+++ 04/Tests.fs
@@ -1008,3 +1008,84 @@ byr:1950
 
 [<Fact>]
 let ``solves 4a problem`` () = solve input2 |> should equal 190
+
+[<Fact>]
+let ``validate fields`` () =
+    validateFieldValue ("byr", "1950") |> should equal true
+    validateFieldValue ("byr", "1900") |> should equal false
+    
+    validateFieldValue ("iyr", "2015") |> should equal true
+    validateFieldValue ("iyr", "1800") |> should equal false
+    
+    validateFieldValue ("eyr", "2025") |> should equal true
+    validateFieldValue ("eyr", "3000") |> should equal false
+    
+    validateFieldValue ("hgt", "5cm") |> should equal false
+    validateFieldValue ("hgt", "175cm") |> should equal true
+    
+    validateFieldValue ("hcl", "#abc123") |> should equal true
+    validateFieldValue ("hcl", "#azc123") |> should equal false
+    validateFieldValue ("hcl", "foobar") |> should equal false
+    
+    validateFieldValue ("ecl", "ZZ") |> should equal false
+    validateFieldValue ("ecl", "gry") |> should equal true
+    
+    validateFieldValue ("pid", "123") |> should equal false
+    validateFieldValue ("pid", "123456789") |> should equal true
+
+[<Fact>]
+let ``validate example fields`` () =
+    validateFieldValue ("byr","2002") |> should equal true
+    validateFieldValue ("byr","2003") |> should equal false
+    validateFieldValue ("hgt","60in") |> should equal true
+    validateFieldValue ("hgt","190cm") |> should equal true
+    validateFieldValue ("hgt","190in") |> should equal false
+    validateFieldValue ("hgt","190") |> should equal false
+
+    validateFieldValue ("hcl","#123abc") |> should equal true
+    validateFieldValue ("hcl","#123abz") |> should equal false
+    validateFieldValue ("hcl","123abc") |> should equal false
+
+    validateFieldValue ("ecl", "brn") |> should equal true
+    validateFieldValue ("ecl","wat") |> should equal false
+
+    validateFieldValue ("pid","000000001") |> should equal true
+    validateFieldValue ("pid","0123456789") |> should equal false
+
+[<Fact>]
+
+let ``solves 4b example 1`` () =
+    let example1 = """
+    eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+
+"""
+    solve2 example1 |> should equal 0
+    
+[<Fact>]
+let ``solves 4b example 2`` () =
+    let example2 = """
+
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+"""
+    solve2 example2 |> should equal 4
+    
+[<Fact>]
+let ``solves 4b `` () = solve2 input2 |> should equal 121
