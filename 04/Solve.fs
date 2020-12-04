d032d218a9ac4b99fdbb2acdee5ce78b95121f40
John Ruble <john.ruble@atomicobject.com>
2020-12-04 00:25:47 -0500

    4a example

--- 04/Solve.fs
+++ 04/Solve.fs
@@ -1,5 +1,28 @@
 module Solve
+
 open System
 
-let solve (input:string) =
-    input
+
+let parse (input: string) =
+    input.Split("\n\n")
+    |> Seq.map (fun s -> s.Trim())
+    |> Seq.map (fun s ->
+        s.Split([| ' '; '\n' |])
+        |> Seq.map (fun s2 -> s2.Split(':'))
+        |> Seq.map (fun a -> (a.[0], a.[1]))
+        |> Array.ofSeq)
+
+let solve (input: string) =
+    let parsed = parse input
+    parsed
+    |> Seq.map (fun r ->
+        let keys =
+            r
+            |> Seq.map fst
+            |> Seq.sort
+            |> Seq.filter (fun s -> s <> "cid")
+            |> Seq.reduce (sprintf "%s-%s")
+
+        String.Equals(keys, "byr-ecl-eyr-hcl-hgt-iyr-pid"))
+    |> Seq.filter id
+    |> Seq.length
--- 04/Tests.fs
+++ 04/Tests.fs
@@ -5,7 +5,42 @@ open Xunit
 open FsUnit.Xunit
 open Solve
 
+let input = """
+ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in
+"""
+
 [<Fact>]
-let ``solves problem`` () =
-    solve "foo" |> should equal "foo"
-    
+let parses () =
+    let smallInput = """
+    ecl:gry pid:860033327
+byr:1937
+
+cid:350
+hcl:#cfa07d byr:1929
+"""
+
+    parse smallInput
+    |> Array.ofSeq
+    |> should
+        equal
+           [| [| ("ecl", "gry")
+                 ("pid", "860033327")
+                 ("byr", "1937") |]
+              [| ("cid", "350")
+                 ("hcl", "#cfa07d")
+                 ("byr", "1929") |] |]
+
+[<Fact>]
+let ``solves 4a example`` () = solve input |> should equal 2
