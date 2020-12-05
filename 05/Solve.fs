b301f0d8f3ecebcca83a5c6cda580dad832e5bb0
John Ruble <john.ruble@atomicobject.com>
2020-12-05 01:27:56 -0500

    solve 5a

--- 05/Solve.fs
+++ 05/Solve.fs
@@ -1,5 +1,22 @@
 module Solve
+
 open System
 
-let solve (input:string) =
-    input
+let solve (input: string) = input
+
+let parseBinary s = Convert.ToInt32(s, 2)
+
+let parse (input: string) =
+    let left =
+        input.Substring(0, 7).Replace('B', '1').Replace('F', '0')
+        |> parseBinary
+
+    let right =
+        input.Substring(7).Replace('R', '1').Replace('L', '0')
+        |> parseBinary
+
+    (left, right)
+
+let seatId (x: int * int) =
+    let (l, r) = x
+    l * 8 + r
