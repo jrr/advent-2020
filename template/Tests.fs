39f928f56657a9652bc5fb13b7865d42cfb01580
John Ruble <john.ruble@atomicobject.com>
2020-12-05 23:47:19 -0500

    update template

--- template/Tests.fs
+++ template/Tests.fs
@@ -6,6 +6,20 @@ open FsUnit.Xunit
 open Solve
 
 [<Fact>]
-let ``solves problem`` () =
-    solve "foo" |> should equal "foo"
-    
+let ``solves problem`` () = solve "foo" |> should equal "foo"
+
+
+
+let input = """FBBBBBBLRR
+FFBBBBFRRL
+FFBBBFBRLR
+BFFBFBFRLL
+BFFFBBFRRR
+FFBFBFFRRR
+BFFBBBBRLL"""
+
+[<Fact>]
+let stuff () =
+    input.Split('\n')
+    |> Seq.length
+    |> should greaterThan 3
