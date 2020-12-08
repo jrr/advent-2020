fd3484c26274e5068840e1a33629bea0e793dd17
John Ruble <john.ruble@atomicobject.com>
2020-12-08 00:41:59 -0500

    halt with more information

--- 08/Tests.fs
+++ 08/Tests.fs
@@ -31,23 +31,24 @@ let ``reads lines of text`` () =
 type MachineState = {iptr : int; accum:int}
 let initialMachineState : MachineState = {iptr=0;accum=0}
 
+type Instruction =string*int
+
 type TerminationCondition =
-    | InfiniteLoop of MachineState
-    | EndOfProgram of MachineState
+    | InfiniteLoop of MachineState*MachineState*Instruction
+    | EndOfProgram of MachineState*MachineState*Instruction
     
 let rec go (instructions: list<string * int>) (machineState:MachineState) (trace:int list) : TerminationCondition =
-    if trace |> List.contains machineState.iptr then
-        InfiniteLoop machineState
-    else
-        let maybeInstruction = instructions |> List.tryItem machineState.iptr
-        match maybeInstruction with
-        | None -> EndOfProgram machineState
-        | Some instruction ->
-            let newState = match instruction with
-                                | "acc",x -> {machineState with iptr = (machineState.iptr+1);accum=machineState.accum + x}
-                                | "jmp",x -> {machineState with iptr = (machineState.iptr+x)}
-                                | "nop",_ -> {machineState with iptr = (machineState.iptr+1)}
-                                | _ -> failwith "unrecognized instruction"
+        let instruction = instructions.[machineState.iptr]
+        let newState = match instruction with
+                            | "acc",x -> {machineState with iptr = (machineState.iptr+1);accum=machineState.accum + x}
+                            | "jmp",x -> {machineState with iptr = (machineState.iptr+x)}
+                            | "nop",_ -> {machineState with iptr = (machineState.iptr+1)}
+                            | _ -> failwith "unrecognized instruction"
+        if instructions |> List.tryItem newState.iptr |> Option.isNone then
+            EndOfProgram (machineState,newState,instruction)
+        else if trace |> List.contains newState.iptr then
+            InfiniteLoop (machineState,newState,instruction)
+        else
             go instructions newState (machineState.iptr::trace)
     
 let execute input = 
@@ -56,13 +57,13 @@ let execute input =
     
 [<Fact>]
 let ``solves 8a example`` () =
-    execute Input.inputLines |> should equal (InfiniteLoop {iptr=1;accum=5})
+    execute Input.inputLines |> should equal ( InfiniteLoop ({ iptr = 4; accum = 5 }, { iptr = 1; accum = 5 }, ("jmp", -3)) )
     
 [<Fact>]
 let ``solves 8a problem`` () =
-    execute Input.input |> should equal (InfiniteLoop {iptr=359;accum=1137})
+    execute Input.input |> should equal (InfiniteLoop ({iptr=344;accum=1137},{iptr=359;accum=1137},("jmp", 15)))
     
 [<Fact>]
 let ``terminates at the end of a program`` () =
-    execute Input.terminatingInput |> should equal (EndOfProgram {iptr=9;accum=8})
+    execute Input.terminatingInput |> should equal (EndOfProgram ({iptr=8;accum=2},{iptr=9;accum=8},("acc",6)))
     
