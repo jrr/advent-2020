948abe6e7850dee3abeaa55761b9106be939075f
John Ruble <john.ruble@atomicobject.com>
2020-12-08 00:21:53 -0500

    8a

--- 08/Solve.fs
+++ 08/Solve.fs
@@ -1,5 +1,11 @@
 module Solve
+
 open System
 
-let solve (input:string) =
-    input
+let solve (input: string) = input
+
+let parse (input: string) =
+    let splits = input.Split(' ')
+    let instr = splits.[0]
+    let operand = splits.[1]
+    (instr, int operand)
--- 08/Tests.fs
+++ 08/Tests.fs
@@ -14,12 +14,46 @@ let ``solves problem`` () = solve "foo" |> should equal "foo"
 let ``reads lines of text`` () =
     Input.inputLines
     |> Common.nonEmptyLines
-    |> Seq.length
-    |> should greaterThan 3
+    |> Seq.map parse
+    |> Seq.toList
+    |> should
+        equal
+           [ ("nop", 0)
+             ("acc", 1)
+             ("jmp", 4)
+             ("acc", 3)
+             ("jmp", -3)
+             ("acc", -99)
+             ("acc", 1)
+             ("jmp", -4)
+             ("acc", 6) ]
 
+type MachineState = {iptr : int; accum:int}
+let initialMachineState : MachineState = {iptr=0;accum=0}
+
+let rec go (instructions: list<string * int>) (machineState:MachineState) (trace:int list) =
+    if trace |> List.contains machineState.iptr then
+        failwith (sprintf "infinite loop detected. accum=%d" machineState.accum)
+    
+    let instruction = instructions.[machineState.iptr]
+    let newState = match instruction with
+                        | "acc",x -> {machineState with iptr = (machineState.iptr+1);accum=machineState.accum + x}
+                        | "jmp",x -> {machineState with iptr = (machineState.iptr+x)}
+                        | "nop",_ -> {machineState with iptr = (machineState.iptr+1)}
+                        | _ -> failwith "unrecognized instruction"
+    go instructions newState (machineState.iptr::trace)
+    ()
+    
 [<Fact>]
-let ``reads groups of lines of text`` () =
-    Input.inputGroups
-    |> Common.lineGroups
-    |> Seq.length
-    |> should equal 2
+let ``solves 8a example`` () =
+    let instructions = Input.inputLines |> Common.nonEmptyLines |> Seq.map parse |> Seq.toList
+    go instructions initialMachineState []
+    // 5
+    ()
+    
+[<Fact>]
+let ``solves 8a problem`` () =
+    let instructions = Input.input |> Common.nonEmptyLines |> Seq.map parse |> Seq.toList
+    go instructions initialMachineState []
+    ()
+    
