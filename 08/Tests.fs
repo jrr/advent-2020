03f8310fce5c424ddb4fc14f33a36895ddcf2267
John Ruble <john.ruble@atomicobject.com>
2020-12-08 00:24:15 -0500

    return termination condition

--- 08/Tests.fs
+++ 08/Tests.fs
@@ -31,29 +31,28 @@ let ``reads lines of text`` () =
 type MachineState = {iptr : int; accum:int}
 let initialMachineState : MachineState = {iptr=0;accum=0}
 
-let rec go (instructions: list<string * int>) (machineState:MachineState) (trace:int list) =
-    if trace |> List.contains machineState.iptr then
-        failwith (sprintf "infinite loop detected. accum=%d" machineState.accum)
+type TerminationCondition =
+    | InfiniteLoop of MachineState
     
-    let instruction = instructions.[machineState.iptr]
-    let newState = match instruction with
-                        | "acc",x -> {machineState with iptr = (machineState.iptr+1);accum=machineState.accum + x}
-                        | "jmp",x -> {machineState with iptr = (machineState.iptr+x)}
-                        | "nop",_ -> {machineState with iptr = (machineState.iptr+1)}
-                        | _ -> failwith "unrecognized instruction"
-    go instructions newState (machineState.iptr::trace)
-    ()
+let rec go (instructions: list<string * int>) (machineState:MachineState) (trace:int list) : TerminationCondition =
+    if trace |> List.contains machineState.iptr then
+        InfiniteLoop machineState
+    else
+        let instruction = instructions.[machineState.iptr]
+        let newState = match instruction with
+                            | "acc",x -> {machineState with iptr = (machineState.iptr+1);accum=machineState.accum + x}
+                            | "jmp",x -> {machineState with iptr = (machineState.iptr+x)}
+                            | "nop",_ -> {machineState with iptr = (machineState.iptr+1)}
+                            | _ -> failwith "unrecognized instruction"
+        go instructions newState (machineState.iptr::trace)
     
 [<Fact>]
 let ``solves 8a example`` () =
     let instructions = Input.inputLines |> Common.nonEmptyLines |> Seq.map parse |> Seq.toList
-    go instructions initialMachineState []
-    // 5
-    ()
+    go instructions initialMachineState [] |> should equal (InfiniteLoop {iptr=1;accum=5})
     
 [<Fact>]
 let ``solves 8a problem`` () =
     let instructions = Input.input |> Common.nonEmptyLines |> Seq.map parse |> Seq.toList
-    go instructions initialMachineState []
-    ()
+    go instructions initialMachineState [] |> should equal (InfiniteLoop {iptr=359;accum=1137})
     
