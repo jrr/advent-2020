adf6236774e921add49300bd50bef2a906b1294c
John Ruble <john.ruble@atomicobject.com>
2020-12-09 00:16:02 -0500

    9a

--- 09/Tests.fs
+++ 09/Tests.fs
@@ -5,10 +5,6 @@ open Xunit
 open FsUnit.Xunit
 open Solve
 
-[<Fact>]
-let ``solves problem`` () = solve "foo" |> should equal "foo"
-
-
 
 [<Fact>]
 let ``reads lines of text`` () =
@@ -23,3 +19,42 @@ let ``reads groups of lines of text`` () =
     |> Common.lineGroups
     |> Seq.length
     |> should equal 2
+
+let validate (preamble:int seq) (candidate:int) =
+    let pairs = Seq.allPairs preamble preamble |> Seq.distinct
+    printfn "pairs %O" pairs
+    
+    match pairs |> Seq.tryFind (fun (a,b) -> a+b = candidate) with
+    | None -> false
+    | Some _ -> true
+    
+[<Fact>]
+let ``validates good`` ()=
+    validate (seq [1;2;3;4;5]) 6 |> should equal true
+    
+[<Fact>]
+let ``validates bad`` ()=
+    validate (seq [1;2;3;4;5]) 15 |> should equal false
+    
+let rec go (nums:int seq) (windowLen:int) =
+    let window = nums |> Seq.take windowLen
+    let candidate = nums |> Seq.skip windowLen |> Seq.head
+    printfn "window %O candidate %O" window candidate
+    let valid = validate window candidate
+    if valid then
+        go (nums |> Seq.skip 1) windowLen
+    else
+        candidate
+    
+[<Fact>]
+let ``solves example`` ()=
+    let intSeq = Input.exampleInput |> Common.nonEmptyLines |> Seq.map int
+    let windowLen = 5
+    go intSeq windowLen |> should equal 127
+    
+[<Fact>]
+let ``solves problem`` ()=
+    let intSeq = Input.problemInput |> Common.nonEmptyLines |> Seq.map int
+    let windowLen = 25
+    go intSeq windowLen |> should equal 507622668
+    
