7c0bc8d951740773a7ccb1263be10987b2d80cec
John Ruble <john.ruble@atomicobject.com>
2020-12-10 00:15:53 -0500

    solve 10a

--- 10/Tests.fs
+++ 10/Tests.fs
@@ -23,3 +23,32 @@ let ``reads groups of lines of text`` () =
     |> Common.lineGroups
     |> Seq.length
     |> should equal 2
+
+let solve10a (input: int seq) =
+    let sorted = input |> Seq.sort
+    let max = sorted |> Seq.max
+    let joltDifferences = sorted
+                                |> Seq.append [ 0; max + 3]
+                                |> Seq.sort
+                                |> Seq.pairwise
+                                |> Seq.map (fun (a, b) -> b - a)
+                                |> Seq.countBy id
+        //    |> Seq.map (fun (count,value)-> 0)
+    let oneJoltDifferences = joltDifferences |> Seq.find (fun (a,_) -> a = 1) |> snd
+    let threeJoltDifferences = joltDifferences |> Seq.find (fun (a,_) -> a = 3) |> snd
+    oneJoltDifferences * threeJoltDifferences
+    
+[<Fact>]
+let ``solves 10a example`` () =
+    solve10a Input.exampleInput  |> should equal 35
+    ()
+    
+[<Fact>]
+let ``solves 10a example2`` () =
+    solve10a Input.example2 |> should equal 220
+    ()
+    
+[<Fact>]
+let ``solves 10a problem`` () =
+    solve10a Input.problemInput |> should equal 2312
+    ()
