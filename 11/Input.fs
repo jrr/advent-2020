a1809421d7c8ff32ed52252ce0f7e42fc084a4b7
John Ruble <john.ruble@atomicobject.com>
2020-12-11 12:53:26 -0500

    parsing

--- 11/Input.fs
+++ 11/Input.fs
@@ -14,3 +14,16 @@ a group
 and this is
 another one
 """
+
+let exampleInput = """
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
+"""
--- 11/Tests.fs
+++ 11/Tests.fs
@@ -23,3 +23,50 @@ let ``reads groups of lines of text`` () =
     |> Common.lineGroups
     |> Seq.length
     |> should equal 2
+    
+type Seat =
+    | Floor
+    | OpenSeat
+    | OccupiedSeat
+    
+let parseCell = function
+    | 'L' -> OpenSeat
+    | '.' -> Floor
+    | '#' -> OccupiedSeat
+    
+let unParseCell = function
+    | OpenSeat -> 'L'
+    | Floor -> '.'
+    | OccupiedSeat -> '#'
+    
+    
+let parse (input: string) : Seat array array =
+    input |> Common.nonEmptyLines |> Seq.map (fun line ->
+        line |> Seq.map (parseCell) |> Array.ofSeq
+        ) |> Array.ofSeq
+let unparse (input: Seat array array) =
+    input |> Seq.map (fun line ->
+        line |> Seq.map unParseCell |> String.Concat
+        ) |> Seq.reduce (sprintf "%s\n%s") |> sprintf "\n%s\n"
+    
+    
+[<Fact>]
+let ``parses board`` () =
+    let result = parse Input.exampleInput
+    result.[1].[1] |> should equal OpenSeat
+    
+[<Fact>]
+let ``parses small board`` () =
+    let result = parse """.L#
+#L.
+"""
+    result |> should equal [|[|Floor;OpenSeat;OccupiedSeat|];[|OccupiedSeat;OpenSeat;Floor|] |]
+    
+[<Fact>]
+let ``unparses small board`` () =
+    
+    [|[|Floor;OpenSeat;OccupiedSeat|];[|OccupiedSeat;OpenSeat;Floor|] |] |> unparse
+        |> should equal """
+.L#
+#L.
+"""
