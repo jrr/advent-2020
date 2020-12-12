417af633693a345f59f39c657438fc0ec0319cbc
John Ruble <john.ruble@atomicobject.com>
2020-12-12 01:54:07 -0500

    cleanup

--- 12/Solve.fs
+++ 12/Solve.fs
@@ -5,7 +5,7 @@ open System
 let solve (input: string) = input
 
 
-type Coords = {X:int;Y:int}
+type Coords = { X: int; Y: int }
 
 open System.Text.RegularExpressions
 
@@ -20,89 +20,146 @@ let parseLine (s: string) =
     | Regex @"(\D)(\d+)" [ letter; num ] -> (letter.[0], int num)
     | _ -> failwith "parse error"
 
-type ShipState = { (* 0 is north, 90 is east *)Facing: int; Coords:Coords}
+type ShipState = { Facing: int; Coords: Coords }
 
-let initialShipState = {Facing=90;Coords={X=0;Y=0}}
+let initialShipState =
+    { Facing = 90
+      Coords = { X = 0; Y = 0 } }
 
-let add (a:Coords) (b:Coords) = {X=a.X+b.X;Y=a.Y+b.Y}
+let add (a: Coords) (b: Coords) = { X = a.X + b.X; Y = a.Y + b.Y }
+
+let North i = { X = 0; Y = i }
+let South i = { X = 0; Y = 0 - i }
+let East i = { X = i; Y = 0 }
+let West i = { X = 0 - i; Y = 0 }
+
 let goForward state i =
     match state.Facing with
-    | 0 -> {state with Coords = add state.Coords {X=0;Y=i}}
-    | 90 -> {state with Coords = add state.Coords {X=i;Y=0}}
-    | 180 -> {state with Coords = add state.Coords {X=0;Y=0-i}}
-    | 270 -> {state with Coords = add state.Coords {X=0-i;Y=0}}
-    
-    
-let rec go (state:ShipState) (instructions: (char*int) seq) =
+    | 0 ->
+        { state with
+              Coords = add state.Coords (North i) }
+    | 90 ->
+        { state with
+              Coords = add state.Coords (East i) }
+    | 180 ->
+        { state with
+              Coords = add state.Coords (South i) }
+    | 270 ->
+        { state with
+              Coords = add state.Coords (West i) }
+    | _ -> failwith "unhandled direction"
+
+
+let reduceShipState (state: ShipState) (instr: char * int) =
+    match instr with
+    | ('N', i) ->
+        { state with
+              Coords = add state.Coords (North i) }
+    | ('E', i) ->
+        { state with
+              Coords = add state.Coords (East i) }
+    | ('S', i) ->
+        { state with
+              Coords = add state.Coords (South i) }
+    | ('W', i) ->
+        { state with
+              Coords = add state.Coords (West i) }
+    | ('L', i) ->
+        { state with
+              Facing = ((state.Facing + 360 - i) % 360) }
+    | ('R', i) ->
+        { state with
+              Facing = ((state.Facing + 360 + i) % 360) }
+    | ('F', i) -> (goForward state i)
+    | x -> failwith (sprintf "unhandled instruction %O" x)
+
+let rec go (state: ShipState) (instructions: (char * int) seq) =
     let tail = instructions |> Seq.tail
-    
+
     match instructions |> Seq.tryHead with
     | None -> state
-    | Some ('N',i) -> go {state with Coords = add state.Coords {X=0;Y=i}} tail
-    | Some ('E',i) -> go {state with Coords = add state.Coords {X=i;Y=0}}tail
-    | Some ('S',i) -> go {state with Coords = add state.Coords {X=0;Y=0-i}} tail
-    | Some ('W',i) -> go {state with Coords = add state.Coords {X=0-i;Y=0}} tail
-    | Some ('L',i) -> go {state with Facing = ((state.Facing + 360 - i) % 360) } tail
-    | Some ('R',i) -> go {state with Facing = ((state.Facing + 360 + i) % 360) } tail
-    | Some ('F',i) -> go (goForward state i) tail
-    | instr -> failwith (sprintf "unhandled instruction %O" instr)
-    
-    
-let abs (input:int) =
-    Math.Abs(input)
-    
+    | Some instr -> go (reduceShipState state instr) tail
+
+let abs (input: int) = Math.Abs(input)
+
 let manhattan (coords) = (abs coords.X) + (abs coords.Y)
-    
-let solveOne (instructions: (char*int) seq) =
-    go initialShipState instructions |> (fun s -> manhattan s.Coords)
 
-type StateTwo = {ShipPos: Coords;Waypoint:Coords}
-let initialStateTwo = {ShipPos={X=0;Y=0};Waypoint={X=10;Y=1}}
+let solveOne (instructions: (char * int) seq) =
+    go initialShipState instructions
+    |> (fun s -> manhattan s.Coords)
 
-let quadrant (c:Coords) =
-    (if c.X > 0 then 1 else -1),(if c.Y > 0 then 1 else -1)
-    
-let rot90 (c:Coords) =
+type StateTwo = { ShipPos: Coords; Waypoint: Coords }
+
+let initialStateTwo =
+    { ShipPos = { X = 0; Y = 0 }
+      Waypoint = { X = 10; Y = 1 } }
+
+let quadrant (c: Coords) =
+    (if c.X > 0 then 1 else -1), (if c.Y > 0 then 1 else -1)
+
+let rot90 (c: Coords) =
     let q = quadrant c
-    let mx,my = match q with
-    | (1,1) -> (1,-1)
-    | (1,-1) -> (-1,-1)
-    | (-1,-1) -> (-1,1)
-    | (-1,1) -> (1,1)
-    {X=(abs c.Y) * mx;Y=(abs c.X) * my}
-    
-    
-let rotateWaypoint (state:StateTwo) (degrees:int) =
-    let dir = (degrees+360) % 360
+
+    let mx, my =
+        match q with
+        | (1, 1) -> (1, -1)
+        | (1, -1) -> (-1, -1)
+        | (-1, -1) -> (-1, 1)
+        | (-1, 1) -> (1, 1)
+        | _ -> failwith "bonk"
+
+    { X = (abs c.Y) * mx
+      Y = (abs c.X) * my }
+
+
+let rotateWaypoint (state: StateTwo) (degrees: int) =
+    let dir = (degrees + 360) % 360
     match dir with
     | 0 -> state
-    | 90 ->  {state with Waypoint= (state.Waypoint|> rot90)}
-    | 180 ->  {state with Waypoint= (state.Waypoint|> rot90|>rot90)}
-    | 270 ->  {state with Waypoint= (state.Waypoint|> rot90|>rot90|>rot90)}
-    
-let mul (a:Coords) (b:int) = {X=a.X*b;Y=a.Y*b}
+    | 90 ->
+        { state with
+              Waypoint = (state.Waypoint |> rot90) }
+    | 180 ->
+        { state with
+              Waypoint = (state.Waypoint |> rot90 |> rot90) }
+    | 270 ->
+        { state with
+              Waypoint = (state.Waypoint |> rot90 |> rot90 |> rot90) }
+    | _ -> failwith "unhandled direction"
 
-let reduce (state:StateTwo) (instr:Char*int) =
+let mul (a: Coords) (b: int) = { X = a.X * b; Y = a.Y * b }
+
+let reduce (state: StateTwo) (instr: Char * int) =
     match instr with
-    | 'N',i -> {state with Waypoint= add state.Waypoint {X=0;Y=i}}
-    | 'S',i -> {state with Waypoint= add state.Waypoint {X=0;Y=0-i}}
-    | 'E',i -> {state with Waypoint= add state.Waypoint {X=i;Y=0}}
-    | 'W',i -> {state with Waypoint= add state.Waypoint {X=0-i;Y=0}}
-    | 'R',i -> rotateWaypoint state i
-    | 'L',i -> rotateWaypoint state (0-i)
-    | 'F',i -> {state with ShipPos = add state.ShipPos (mul state.Waypoint i)}
-    | _-> failwith "bonk"
-    
-    
-let rec go2 (state:StateTwo) (instructions: (char*int) seq) =
+    | 'N', i ->
+        { state with
+              Waypoint = add state.Waypoint (North i) }
+    | 'S', i ->
+        { state with
+              Waypoint = add state.Waypoint (South i) }
+    | 'E', i ->
+        { state with
+              Waypoint = add state.Waypoint (East i) }
+    | 'W', i ->
+        { state with
+              Waypoint = add state.Waypoint (West i) }
+    | 'R', i -> rotateWaypoint state i
+    | 'L', i -> rotateWaypoint state (0 - i)
+    | 'F', i ->
+        { state with
+              ShipPos = add state.ShipPos (mul state.Waypoint i) }
+    | _ -> failwith "bonk"
+
+
+let rec go2 (state: StateTwo) (instructions: (char * int) seq) =
     let tail = instructions |> Seq.tail
-    
+
     match instructions |> Seq.tryHead with
     | None -> state
-    | Some instr-> go2 (reduce state instr) tail
-    
-    
-    
-let solveTwo (instructions: (char*int) seq) = 
+    | Some instr -> go2 (reduce state instr) tail
+
+
+
+let solveTwo (instructions: (char * int) seq) =
     let endState = go2 initialStateTwo instructions
     endState |> (fun s -> manhattan s.ShipPos)
--- 12/Tests.fs
+++ 12/Tests.fs
@@ -10,8 +10,8 @@ let ``solves problem`` () = solve "foo" |> should equal "foo"
 
 
 
-type ``Helpers`` () =
-    
+type Helpers() =
+
     [<Fact>]
     let ``reads lines of text`` () =
         Input.inputLines
@@ -26,41 +26,81 @@ type ``Helpers`` () =
         |> Seq.length
         |> should equal 2
 
-type ``Part One`` () =
+type ``Part One``() =
     [<Fact>]
     let ``parses line`` () =
-        Input.exampleInput |> Common.nonEmptyLines |> Seq.head |> parseLine |> should equal ('F',10)
-        
+        Input.exampleInput
+        |> Common.nonEmptyLines
+        |> Seq.head
+        |> parseLine
+        |> should equal ('F', 10)
+
     [<Fact>]
     let ``solves example`` () =
-        Input.exampleInput |> Common.nonEmptyLines |> Seq.map parseLine
-            |>solveOne |> should equal 25
+        Input.exampleInput
+        |> Common.nonEmptyLines
+        |> Seq.map parseLine
+        |> solveOne
+        |> should equal 25
+
     [<Fact>]
     let ``solves problem`` () =
-        Input.problemInput |> Common.nonEmptyLines |> Seq.map parseLine
-            |> solveOne |> should equal 1687
-        
-type ``Part Two`` () =
+        Input.problemInput
+        |> Common.nonEmptyLines
+        |> Seq.map parseLine
+        |> solveOne
+        |> should equal 1687
+
+type ``Part Two``() =
     [<Fact>]
     let ``reduce 1`` () =
-        reduce initialStateTwo ('F',10) |> should equal {initialStateTwo with ShipPos={X=100;Y=10}}
-        
+        reduce initialStateTwo ('F', 10)
+        |> should
+            equal
+               { initialStateTwo with
+                     ShipPos = { X = 100; Y = 10 } }
+
     [<Fact>]
     let ``reduce 2`` () =
-        reduce {ShipPos={X=100;Y=10};Waypoint={X=10;Y=1}} ('N',3) |> should equal {ShipPos={X=100;Y=10};Waypoint={X=10;Y=4}}
+        reduce
+            { ShipPos = { X = 100; Y = 10 }
+              Waypoint = { X = 10; Y = 1 } }
+            ('N', 3)
+        |> should
+            equal
+               { ShipPos = { X = 100; Y = 10 }
+                 Waypoint = { X = 10; Y = 4 } }
+
     [<Fact>]
-    let ``rot90`` () =
-        rot90 {X=10;Y=4} |> should equal {X=4;Y=0-10}
-        rot90 {X=1;Y=0-2} |> should equal {X=0-2;Y=0-1}
-        
+    let rot90 () =
+        rot90 { X = 10; Y = 4 }
+        |> should equal { X = 4; Y = 0 - 10 }
+        rot90 { X = 1; Y = 0 - 2 }
+        |> should equal { X = 0 - 2; Y = 0 - 1 }
+
     [<Fact>]
     let ``reduce 3`` () =
-        reduce {ShipPos={X=100;Y=10};Waypoint={X=10;Y=4}} ('R',90) |> should equal {ShipPos={X=100;Y=10};Waypoint={X=4;Y=0-10}}
-        
+        reduce
+            { ShipPos = { X = 100; Y = 10 }
+              Waypoint = { X = 10; Y = 4 } }
+            ('R', 90)
+        |> should
+            equal
+               { ShipPos = { X = 100; Y = 10 }
+                 Waypoint = { X = 4; Y = 0 - 10 } }
+
     [<Fact>]
     let ``solves example`` () =
-        Input.exampleInput |> Common.nonEmptyLines |> Seq.map parseLine |> solveTwo |> should equal 286
-        
+        Input.exampleInput
+        |> Common.nonEmptyLines
+        |> Seq.map parseLine
+        |> solveTwo
+        |> should equal 286
+
     [<Fact>]
     let ``solves problem`` () =
-        Input.problemInput |> Common.nonEmptyLines |> Seq.map parseLine |> solveTwo |> should equal 20873
+        Input.problemInput
+        |> Common.nonEmptyLines
+        |> Seq.map parseLine
+        |> solveTwo
+        |> should equal 20873
