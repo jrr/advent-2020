a2ab18d867df762526eea34510830bdf8d8fdfac
John Ruble <john.ruble@atomicobject.com>
2020-12-14 11:19:33 -0500

    14a

--- 14/Solve.fs
+++ 14/Solve.fs
@@ -13,22 +13,42 @@ let parseLine (s: string) =
     //mem[8] = 0
     match s with
     | Regex @"mask = ([01X]*)$" [ mask ] -> Mask mask
-    | Regex @"mem\[(\d)\] = (\d+)" [ address; value ] -> Write(uint address, uint64 value)
+    | Regex @"mem\[(\d+)\] = (\d+)" [ address; value ] -> Write(uint address, uint64 value)
     | x -> failwith $"parse error ({x})"
-    
+
 let parse (input: string) =
     input |> Common.nonEmptyLines |> Seq.map parseLine
-    
+
 open System
 
-let parseBinary (input:string) =
-    Convert.ToUInt64(input, 2);
+let parseBinary (input: string) = Convert.ToUInt64(input, 2)
 
-let applyMask (value:uint64) (mask:string) =
-    let orMask = mask.Replace('X','0') |> parseBinary
-    let andMask = mask.Replace('X','1') |> parseBinary
+let applyMask (value: uint64) (mask: string) =
+    let orMask = mask.Replace('X', '0') |> parseBinary
+    let andMask = mask.Replace('X', '1') |> parseBinary
     (value &&& andMask) ||| orMask
 
-let solveOne (input: string) = input
+let rec applyMasksToLinesInner (input: ParsedLine seq) (currentMask: string) =
+    let head = input |> Seq.tryHead
+    match head with
+    | None -> []
+    | Some (Mask m) -> applyMasksToLinesInner (input |> Seq.tail) m
+    | Some (Write (a, b)) ->
+        (a, (applyMask b currentMask))
+        :: (applyMasksToLinesInner (input |> Seq.tail) currentMask)
+
+
+let applyMasksToLines (input: ParsedLine seq) = applyMasksToLinesInner input "X"
+
+let reduceWrites (input: (uint * uint64) seq) =
+    input |> Seq.rev |> Seq.distinctBy fst |> Seq.rev
+
+let solveOne (input: string) =
+    input
+    |> parse
+    |> applyMasksToLines
+    |> reduceWrites
+    |> Seq.map snd
+    |> Seq.sum
 
 let solveTwo (input: string) = input
--- 14/Tests.fs
+++ 14/Tests.fs
@@ -33,27 +33,42 @@ type Helpers() =
                  Write(8u, 11UL)
                  Write(7u, 101UL)
                  Write(8u, 0UL) ]
-               
+
     [<Fact>]
-    let ``parses binary`` () =
-        "101" |> parseBinary |> should equal 5UL
-        
+    let ``parses binary`` () = "101" |> parseBinary |> should equal 5UL
+
     [<Fact>]
     let ``applies mask`` () =
-        applyMask 11UL "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X" |> should equal 73UL
-        applyMask 00UL "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X" |> should equal 64UL
-        
+        applyMask 11UL "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
+        |> should equal 73UL
+        applyMask 00UL "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
+        |> should equal 64UL
+
+    [<Fact>]
+    let ``applies masks`` () =
+        Input.exampleInput
+        |> parse
+        |> applyMasksToLines
+        |> Seq.toList
+        |> should equal [ (8u, 73UL); (7u, 101UL); (8u, 64UL) ]
+
+    [<Fact>]
+    let ``reduces redundant writes`` () =
+        [ (8u, 73UL); (7u, 101UL); (8u, 64UL) ]
+        |> reduceWrites
+        |> Seq.toList
+        |> should equal [ (7u, 101UL); (8u, 64UL) ]
+
 
 type ``Part One``() =
     [<Fact>]
     let ``solves example`` () =
-        solveOne Input.exampleInput
-        |> should equal Input.exampleInput
+        solveOne Input.exampleInput |> should equal 165UL
 
     [<Fact>]
     let ``solves problem`` () =
-        solveTwo Input.problemInput
-        |> should equal Input.problemInput
+        solveOne Input.problemInput
+        |> should equal 7817357407588UL
 
 type ``Part Two``() =
     [<Fact>]
