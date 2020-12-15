f3829e3f0af9bd31575ce1091fb75d5421725390
John Ruble <john.ruble@atomicobject.com>
2020-12-15 01:53:09 -0500

    15a

--- 15/Solve.fs
+++ 15/Solve.fs
@@ -1,5 +1,25 @@
 module Solve
 
-let solveOne (input: string) = input
+let rec recSeq (input: int list) =
+    seq {
+        let head :: tail = input
+
+        let next =
+            match tail |> Seq.tryFindIndex (fun x -> x = head) with
+            | Some previousUse -> previousUse + 1
+            | None -> 0
+
+        yield next
+        yield! recSeq (next :: input)
+    }
+
+let buildSeq (input: int list) = recSeq (input |> List.rev)
+
+let solveOne (input: int list) =
+    input
+    |> buildSeq
+    |> Seq.skip (2020 - input.Length - 1)
+    |> Seq.take 1
+    |> Seq.head
 
 let solveTwo (input: string) = input
--- 15/Tests.fs
+++ 15/Tests.fs
@@ -24,16 +24,32 @@ type ``Helpers`` () =
 
 type ``Part One`` () =
     [<Fact>]
-    let ``solves example`` () =
-        solveOne Input.exampleInput |> should equal Input.exampleInput
+    let ``small example next digits`` () =
+        let s = buildSeq [0;3;6]
+        s |> Seq.take 2 |> List.ofSeq |> should equal [0;3]
+        s |> Seq.take 7 |> List.ofSeq |> should equal [0;3;3;1;0;4;0]
+        
+    [<Fact>]
+    let ``solves small example`` () =
+        [0;3;6] |> solveOne |> should equal 436
+       
+    [<Fact>]
+    let ``more examples`` () =
+        [1;3;2] |> solveOne |> should equal 1
+        [2;1;3] |> solveOne |> should equal 10
+        [1;2;3] |> solveOne |> should equal 27
+        [2;3;1] |> solveOne |> should equal 78
+        [3;2;1] |> solveOne |> should equal 438
+        [3;1;2] |> solveOne |> should equal 1836
+        
     [<Fact>]
     let ``solves problem`` () =
-        solveTwo Input.problemInput |> should equal Input.problemInput
+        [14;8;16;0;1;17] |> solveOne |> should equal 240
         
 type ``Part Two`` () =
     [<Fact>]
     let ``solves example`` () =
-        solveOne Input.exampleInput |> should equal Input.exampleInput
+        solveTwo Input.exampleInput |> should equal Input.exampleInput
     [<Fact>]
     let ``solves problem`` () =
         solveTwo Input.problemInput |> should equal Input.problemInput
