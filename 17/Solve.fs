566991daf995bf7894087002b97189f5f4787030
John Ruble <john.ruble@atomicobject.com>
2020-12-17 17:06:30 -0500

    compute 2d neighbors

--- 17/Solve.fs
+++ 17/Solve.fs
@@ -1,19 +1,43 @@
 module Solve
 
-type Point = int*int
-        
+type Point = int * int
+
 //      maybe if needed for optimization later, but for now using them is harder
 //        struct
 //                val X: int
 //                val Y: int
 //        new(x, y) = { X = x; Y = y }
 //        end
-        
-let parseLine (input:string) (y:int) : Point seq = input |> Seq.mapi (fun x c -> if (c = '#') then Some (x,y) else None) |> Seq.choose id
 
-let parse (input:string) =
-        input |> Common.nonEmptyLines |> Seq.mapi (fun i line -> parseLine line i) |> Seq.concat |> Set.ofSeq
-        
+let parseLine (input: string) (y: int): Point seq =
+    input
+    |> Seq.mapi (fun x c -> if (c = '#') then Some(x, y) else None)
+    |> Seq.choose id
+
+let parse (input: string) =
+    input
+    |> Common.nonEmptyLines
+    |> Seq.mapi (fun i line -> parseLine line i)
+    |> Seq.concat
+    |> Set.ofSeq
+
+let neighborVectors =
+    [| (-1, -1)
+       (-1, 0)
+       (-1, 1)
+       (0, -1) (*0,0*)
+       (0, 1)
+       (1, -1)
+       (1, 0)
+       (1, 1) |]
+
+let addPoint p1 p2 = (fst p1 + fst p2), (snd p1 + snd p2)
+
+let neighbors (p: int * int) =
+    neighborVectors
+    |> Array.map (fun n -> addPoint n p)
+    |> Set.ofArray
+
 let solveOne (input: string) = input
 
 let solveTwo (input: string) = input
--- 17/Tests.fs
+++ 17/Tests.fs
@@ -6,31 +6,48 @@ open FsUnit.Xunit
 open Solve
 
 
-type ``Helpers`` () =
-    
+type Helpers() =
+
     [<Fact>]
     let ``parses line`` () =
-        parseLine "#..#" 2 |> Seq.toList |> should equal [(0,2);(3,2)]
-        
+        parseLine "#..#" 2
+        |> Seq.toList
+        |> should equal [ (0, 2); (3, 2) ]
+
     [<Fact>]
     let ``parses input`` () =
         Input.exampleInput
         |> parse
-        |> should equal (set [(0, 2); (2, 1); (1, 0); (1, 2); (2, 2)])
+        |> should
+            equal
+               (set [ (0, 2)
+                      (2, 1)
+                      (1, 0)
+                      (1, 2)
+                      (2, 2) ])
 
+    [<Fact>]
+    let neighbors () =
+        (3, 3) |> neighbors |> should equal ( set [(2, 2); (2, 3); (2, 4); (3, 2); (3, 4); (4, 2); (4, 3); (4, 4)] )
 
-type ``Part One`` () =
+type ``Part One``() =
     [<Fact>]
     let ``solves example`` () =
-        solveOne Input.exampleInput |> should equal Input.exampleInput
+        solveOne Input.exampleInput
+        |> should equal Input.exampleInput
+
     [<Fact>]
     let ``solves problem`` () =
-        solveOne Input.problemInput |> should equal Input.problemInput
-        
-type ``Part Two`` () =
+        solveOne Input.problemInput
+        |> should equal Input.problemInput
+
+type ``Part Two``() =
     [<Fact>]
     let ``solves example`` () =
-        solveTwo Input.exampleInput |> should equal Input.exampleInput
+        solveTwo Input.exampleInput
+        |> should equal Input.exampleInput
+
     [<Fact>]
     let ``solves problem`` () =
-        solveTwo Input.problemInput |> should equal Input.problemInput
+        solveTwo Input.problemInput
+        |> should equal Input.problemInput
