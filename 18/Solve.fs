0f43395a6c17cf2765f37f10de600ac5c5fb54d1
John Ruble <john.ruble@atomicobject.com>
2020-12-18 13:19:29 -0500

    18a

--- 18/Solve.fs
+++ 18/Solve.fs
@@ -1,5 +1,80 @@
 module Solve
+open System
+open Common
 
-let solveOne (input: string) = input
+    
+type Token =
+    | IntLiteral of int64
+    | Plus
+    | MultipliedBy
+//    | LeftParen
+//    | RightParen
+    
+let regexParse (s:char list) =
+    let str = s |> String.Concat
+    match str with
+    | Regex @"(\d+)(.*)" [ num;tail ] -> num,tail
+    | x -> failwith (sprintf "parse error on '%s'" x)
+    
+let rec tokenizeRec (s:char list) =
+    match s with
+    | '+'::tail -> Plus::(tokenizeRec tail)
+    | '*'::tail -> MultipliedBy::(tokenizeRec tail)
+//    | '(' -> Some LeftParen
+//    | ')' -> Some RightParen
+    | ' '::tail -> tokenizeRec tail
+    | [] -> []
+    | other ->
+        let n,tail = regexParse other
+        (IntLiteral (int64 n))::(tail |> Seq.toList |> tokenizeRec)
+let tokenize (s:string) =
+    s |> List.ofSeq |> tokenizeRec
+    
+type ParenSplit = {left:string;parenthesized:string;right:string}
+let extractParenthesized (input:string) =
+    match input with
+    | Regex @"^(.*)\(([^\)]+)\)(.*)$" [left;parenthesized;right] -> {left=left;parenthesized=parenthesized;right=right}
+    | _ -> failwith "failed to parse parens"
+//        | "hcl", Regex @"#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]" [] -> true
+    
+    
+type ParenCount =
+    | HasParens of int
+    | NoParens
+    
+let countParens s =
+    let left = s |> Seq.filter (fun c -> c = '(') |> Seq.length
+    let right = s |> Seq.filter (fun c -> c = ')') |> Seq.length
+    match left,right with
+    | 0,0 -> NoParens
+    | l,r when l = r -> HasParens l
+    | l,r when l <> r -> failwith "Mismatched parens"
+    
+let rec eval (input:Token list) =
+    match input with
+    | [IntLiteral n] -> n
+    | (IntLiteral n)::Plus::tail -> n + (eval tail)
+    | (IntLiteral n)::MultipliedBy::tail -> n * (eval tail)
+    | x -> failwith (sprintf "unrecognized token %O" x)
+    
+let evalString (input:string) =
+    let tokens = input |> tokenize |> Seq.toList |> List.rev
+    eval tokens |> (sprintf "%d")
+    
+let rec recSolve (input:string) =
+    match countParens input with
+    | NoParens -> evalString input
+    | HasParens _ ->
+        let splits = extractParenthesized input
+        let solvedMiddle = evalString splits.parenthesized
+        let newString = sprintf "%s%s%s" splits.left solvedMiddle splits.right
+        recSolve newString
+        
+let solveLine (input:string) =
+    input |> recSolve |> int64
+    
+
+let solveOne (input: string) =
+    input |> Common.nonEmptyLines |> Seq.map solveLine  |> Seq.reduce (+)
 
 let solveTwo (input: string) = input
--- 18/Tests.fs
+++ 18/Tests.fs
@@ -6,8 +6,8 @@ open FsUnit.Xunit
 open Solve
 
 
-type ``Helpers`` () =
-    
+type Helpers() =
+
     [<Fact>]
     let ``reads lines of text`` () =
         Input.inputLines
@@ -21,19 +21,76 @@ type ``Helpers`` () =
         |> Common.lineGroups
         |> Seq.length
         |> should equal 2
-
-type ``Part One`` () =
-    [<Fact>]
-    let ``solves example`` () =
-        solveOne Input.exampleInput |> should equal Input.exampleInput
-    [<Fact>]
-    let ``solves problem`` () =
-        solveOne Input.problemInput |> should equal Input.problemInput
         
-type ``Part Two`` () =
     [<Fact>]
-    let ``solves example`` () =
-        solveTwo Input.exampleInput |> should equal Input.exampleInput
+    let ``evaluates single addition`` () =
+        "5+2" |> evalString |> should equal "7"
+    [<Fact>]
+    let ``evaluates multiple operations`` () =
+        "1+2*3" |> evalString |> should equal "9"
+        
+    [<Fact>]
+    let ``solves with parens`` () =
+        "1+(2*3)" |> recSolve |> should equal "7"
+        
+    [<Fact>]
+    let ``tokenizes simple case`` () =
+        "1+2" |>tokenize |> should equal [IntLiteral 1L; Plus; IntLiteral 2L]
+    [<Fact>]
+    let ``tokenizes larger number`` () =
+        "123" |>tokenize |> should equal [IntLiteral 123L]
+        
+    
+
+type ``Part One``() =
+//    [<Fact>]
+//    let ``tokenizes string`` () =
+//        "2 * 3 + (4 * 5)"
+//        |> parse
+//        |> should
+//            equal
+//               [ IntLiteral 2
+//                 MultipliedBy
+//                 IntLiteral 3
+//                 Plus
+//                 LeftParen
+//                 IntLiteral 4
+//                 MultipliedBy
+//                 IntLiteral 5
+//                 RightParen ]
+
+    [<Fact>]
+    let ``counts parens`` () =
+        "123" |> countParens |> should equal NoParens
+        "1(2)3" |> countParens |> should equal (HasParens 1)
+        "(1(2)3)" |> countParens |> should equal (HasParens 2)
+        
+    [<Fact>]
+    let ``extracts single parenthesized sequence`` () =
+        "1+2+(3*4)+5" |> extractParenthesized |> should equal {left="1+2+";parenthesized="3*4";right="+5" }
+        
+    [<Fact>]
+    let ``extracts two parenthesized sequences`` () =
+        "1+2+(3*(4+5))" |> extractParenthesized |> should equal { left = "1+2+(3*"; parenthesized = "4+5"; right = ")" }
+        
+    [<Fact>]
+    let ``solves example lines`` () =
+        "2 * 3 + (4 * 5)" |> solveLine |> should equal 26L
+        "5 + (8 * 3 + 9 + 3 * 4 * 3)" |> solveLine |> should equal 437L
+        "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" |>solveLine |> should equal 12240L
+        "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" |> solveLine |> should equal 13632L
+        
     [<Fact>]
     let ``solves problem`` () =
-        solveTwo Input.problemInput |> should equal Input.problemInput
+        Input.problemInput |> solveOne |> should equal 5019432542701L
+
+type ``Part Two``() =
+    [<Fact>]
+    let ``solves example`` () =
+        solveTwo Input.exampleInput
+        |> should equal Input.exampleInput
+
+    [<Fact>]
+    let ``solves problem`` () =
+        solveTwo Input.problemInput
+        |> should equal Input.problemInput
