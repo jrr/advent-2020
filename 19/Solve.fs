e7ab0200cc06ced1a85d767ad3703fd48e14935c
John Ruble <john.ruble@atomicobject.com>
2020-12-19 01:21:43 -0500

    more

--- 19/Solve.fs
+++ 19/Solve.fs
@@ -4,10 +4,14 @@ open System
 open System.Text.RegularExpressions
 open Common
 
+type NumOrString =
+    | Num of int
+    | String of string
+    
 type Rule =
     | StringRule of string
-    | Digits of int list
-    | Or of (int list) * (int list)
+    | Digits of NumOrString list
+    | Or of (NumOrString list) * (NumOrString list)
 
 type ProblemInput =
     { rules: (int * Rule) list
@@ -15,24 +19,24 @@ type ProblemInput =
 
 let parseLineOfInts (s: string) =
     printfn "parse ints %s" s
-    s.Split(" ") |>Seq.filter (fun x -> x <> "")|> Seq.map int |> Seq.toList
+    s.Split(" ") |>Seq.filter (fun x -> x <> "")|> Seq.map int |> Seq.map Num |>Seq.toList
 
 let parseOrLine (s:string) =
     let splits = s.Split("|")
     (parseLineOfInts splits.[0]),(parseLineOfInts splits.[1])
-    
 
 let parseRule (input: string) =
     match input with
-    | Regex @"^(\d+): ([\d ]+)$" [ i; s ] -> (i, (Digits(parseLineOfInts s)))
-    | Regex @"^(\d+): ([\d \|]+)$" [ i; s ] -> (i, (Or (parseOrLine s)))
-    | Regex """^(\d+): "([ab])"$""" [ i; s ] -> (i, StringRule s)
+    | Regex @"^(\d+): ([\d ]+)$" [ i; s ] -> (int i, (Digits(parseLineOfInts s)))
+    | Regex @"^(\d+): ([\d \|]+)$" [ i; s ] -> (int i, (Or (parseOrLine s)))
+    | Regex """^(\d+): "([ab])"$""" [ i; s ] -> (int i, StringRule s)
     | z -> failwith (sprintf "unrecognized input %s" z)
 
 let parse (input: string) =
-    input
-    // |> Common.lineGroups
-    |> Common.nonEmptyLines
+    let groups = input |> Common.lineGroups |> Seq.toList
+    let rules = groups.[0] |> Seq.map parseRule |> List.ofSeq
+    let messages = groups.[1] |> Seq.toList
+    {rules = rules;messages = messages}
 
 let replaceNumWithString (input: string) (num: int) (replacement: string) =
     Regex.Replace(input, $"([^\d]){num}([^^\d:])", $"$1{replacement}$2")
--- 19/Tests.fs
+++ 19/Tests.fs
@@ -11,23 +11,30 @@ type Helpers() =
 
     [<Fact>]
     let ``parses rules`` () =
-        "60: 117 7 | 89 13" |>parseRule|> should equal ("60", Or ([117; 7], [89; 13]))
-        "64: 99 7"|>parseRule|>should equal ("64", Digits [99; 7])
-        "13: \"b\"" |>parseRule |> should equal ("13", StringRule "b")
-        
+        "60: 117 7 | 89 13"
+        |> parseRule
+        |> should equal (60, Or([ Num 117; Num 7 ], [ Num 89; Num 13 ]))
+        "64: 99 7"
+        |> parseRule
+        |> should equal (64, Digits [ Num 99; Num 7 ])
+        "13: \"b\""
+        |> parseRule
+        |> should equal (13, StringRule "b")
+
     [<Fact>]
     let ``replaces single-digit numbers with letters`` () =
-        
-//        7: "a"
+
+        //        7: "a"
         let input = """5: 108 13 | 27 7
 30: 14 7 | 50 13
 7: but not this one
 50: 13 23 | 7 111
 32: 13 7 | 13 13
 92: 114 13 | 125 7"""
-//        input.repl
+        //        input.repl
         let result = replaceNumWithString input 7 "a"
-        result |> should equal """5: 108 13 | 27 a
+        result
+        |> should equal """5: 108 13 | 27 a
 30: 14 a | 50 13
 7: but not this one
 50: 13 23 | a 111
@@ -36,27 +43,41 @@ type Helpers() =
 
     [<Fact>]
     let ``replaces two-digit numbers with letters`` () =
-        
-//        7: "a"
+
+        //        7: "a"
         let input = """5: 108 13 | 27 7
 30: 14 7 | 50 13
 50: 13 23 | 7 111
 32: 13 7 | 13 13
 92: 114 13 | 125 7"""
-//        input.repl
+        //        input.repl
         let result = replaceNumWithString input 13 "b"
-        result |> should equal "5: 108 b | 27 7
+        result
+        |> should equal "5: 108 b | 27 7
 30: 14 7 | 50 b
 50: b 23 | 7 111
 32: b 7 | b 13
 92: 114 b | 125 7"
-        
+
     [<Fact>]
     let ``parses example input`` () =
         Input.exampleInput
         |> parse
-        |> should not' (equal true)
-        ()
+        |> should
+            equal
+               { rules =
+                     [ (0, Digits [ Num 4; Num 1; Num 5 ])
+                       (1, Or([ Num 2; Num 3 ], [ Num 3; Num 2 ]))
+                       (2, Or([ Num 4; Num 4 ], [ Num 5; Num 5 ]))
+                       (3, Or([ Num 4; Num 5 ], [ Num 5; Num 4 ]))
+                       (4, StringRule "a")
+                       (5, StringRule "b") ]
+                 messages =
+                     [ "ababbb"
+                       "bababa"
+                       "abbbab"
+                       "aaabbb"
+                       "aaaabbb" ] }
 
 type ``Part One``() =
     [<Fact>]
